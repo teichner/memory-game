a4b40125b96ed898c6218d4b517788b2c57ec3b1
Timothy Eichner <tim@Timothys-MacBook-2.local>
2021-07-23 16:11:00 -0400

    Updated card styles

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -16,6 +16,11 @@ export class GameComponent implements OnInit {
   @Input('rowCount') rowCount: number = 4;
   @Input('columnCount') columnCount: number = 6;
   @Input('matchSize') matchSize: number = 2;
+  @Input('cardSize') cardSize: number = 150;
+  @Input('frameBorder') frameBorder: number = 5;
+  @Input('circleBorder') circleBorder: number = 10;
+  @Input('cardSpacing') cardSpacing: number = 20;
+  @Input('patternCount') patternCount: number = 10;
 
   constructor(
     private gameService: GameService
@@ -31,12 +36,75 @@ export class GameComponent implements OnInit {
   }
 
   rowTransform(index: number): string {
-    return `translate(0, ${index * 200})`;
+    return `translate(0, ${index * (this.cardSpacing + this.cardSize)})`;
   }
 
   cellTransform(index: number): string {
-    return `translate(${index * 150}, 0)`;
+    return `translate(${index * (this.cardSpacing + this.cardSize)}, 0)`;
   }
+
+  get circleX(): number {
+    return this.cardSize / 2;
+  }
+
+  get circleY(): number {
+    return this.cardSize / 2;
+  }
+
+  get circleRadius(): number {
+    return (this.cardSize / 2) - this.frameBorder - this.circleBorder;
+  }
+
+  get frameOffset(): number {
+    return this.frameBorder;
+  }
+
+  get frameSize(): number {
+    return this.cardSize - (2 * this.frameBorder);
+  }
+
+  get patternSize(): number {
+    return this.frameSize / this.patternCount;
+  }
+
+  get patternViewBox(): string {
+    const coords = [0, 0, this.patternSize, this.patternSize];
+    return coords.join(',');
+  }
+
+  get patternPercent(): string {
+    const percent = 100 / this.patternCount;
+    return `${percent}%`;
+  }
+
+  get patternPolygonPoints(): string {
+    const size = this.patternSize;
+    const points = [
+      [0, size / 2],
+      [size / 2, 0],
+      [size, size / 2],
+      [size / 2, size]
+    ];
+    return points.map((point) => point.join(',')).join(' ');
+  }
+
+  cardColor(card: Card): string {
+    const highestValue = this.#game.cards.length / this.matchSize;
+    const hue = 360 * (card.value / highestValue);
+    return `hsl(${hue}, 70%, 50%)`
+  }
+
+  bodyFill(card: Card): string {
+    return (card.state === CardState.FACE_UP) ? this.cardColor(card) : 'url(#diamond)';
+  }
+
+  // //TEST
+  // get cardSizeCombo(): any {
+  //   return {
+  //     width: this.cardSize,
+  //     height: this.cardSize
+  //   };
+  // }
 
   get rows(): Card[][] {
     const rows = [];
